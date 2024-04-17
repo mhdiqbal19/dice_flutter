ba67cbd2b3f22da371620ae2508b6636bb5d9748
mhdiqbal19 <muhammadiqbal190794@gmail.com>
2024-04-17 13:15:14 +0700

    first commit

---
+++ Documents/ Flutter Development Bootcamp with Dart/dicee_flutter/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: Text(
+          "Dicee",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage("images/dice${leftDiceNumber}.png"),
+                ),
+              ),
+            ),
+          ), // Added comma here
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage("images/dice${rightDiceNumber}.png"),
+                ),
+              ),
+            ),
+          ), // Added comma here
+        ],
+      ),
+    );
+  }
+}
